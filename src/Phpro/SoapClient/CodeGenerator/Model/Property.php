b2f769acf11c5edd6a692ce31599c09fdf0fabc5
Arne De Smedt <arnedesmdt@gmail.com>
2020-06-25 16:55:45 +0200

    add is nullable check

--- src/Phpro/SoapClient/CodeGenerator/Model/Property.php
+++ src/Phpro/SoapClient/CodeGenerator/Model/Property.php
@@ -74,6 +74,11 @@ class Property
         return '\\'.$this->namespace.'\\'.Normalizer::normalizeClassname($this->type);
     }
 
+    public function isNullable(): bool
+    {
+        return strpos($this->type, '?') === 0;
+    }
+
     /**
      * @return non-empty-string|null
      */
