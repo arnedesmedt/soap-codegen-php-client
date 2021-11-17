a9286c59e0ec47672f07bc6a1d3e2efa2e480402
Jachim Coudenys <jachim.coudenys@combellgroup.com>
2021-11-17 09:59:26 +0100

    The class will be removed in v2.0, FQCN is not working at the moment

--- src/Phpro/SoapClient/Event/Subscriber/LogSubscriber.php
+++ src/Phpro/SoapClient/Event/Subscriber/LogSubscriber.php
@@ -88,9 +88,9 @@ class LogSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return array(
-            RequestEvent::class  => 'onClientRequest',
-            ResponseEvent::class => 'onClientResponse',
-            FaultEvent::class => 'onClientFault'
+            Events::REQUEST  => 'onClientRequest',
+            Events::RESPONSE => 'onClientResponse',
+            Events::FAULT => 'onClientFault'
         );
     }
 }
