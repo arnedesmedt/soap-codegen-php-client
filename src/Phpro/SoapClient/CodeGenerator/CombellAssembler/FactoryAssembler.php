2a059dcef8b9e44e8276b0a42591bf1f7f890658
Arne De Smedt <arnedesmdt@gmail.com>
2022-05-04 15:33:49 +0200

    correct factory assembler

--- src/Phpro/SoapClient/CodeGenerator/CombellAssembler/FactoryAssembler.php
+++ src/Phpro/SoapClient/CodeGenerator/CombellAssembler/FactoryAssembler.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace Phpro\SoapClient\CodeGenerator\CombellAssembler;
 
-use ADS\Util\StringUtil;
 use Exception;
 use Laminas\Code\Generator\MethodGenerator;
-use Laminas\Code\Generator\PropertyGenerator;
 use Phpro\SoapClient\CodeGenerator\Assembler\AssemblerInterface;
+use Phpro\SoapClient\CodeGenerator\Assembler\TraitAssembler;
 use Phpro\SoapClient\CodeGenerator\Context\ContextInterface;
-use Phpro\SoapClient\CodeGenerator\Context\PropertyContext;
 use Phpro\SoapClient\CodeGenerator\Context\TypeContext;
 use Phpro\SoapClient\CodeGenerator\LaminasCodeFactory\DocBlockGeneratorFactory;
-use Phpro\SoapClient\CodeGenerator\Util\Normalizer;
 use Phpro\SoapClient\Exception\AssemblerException;
 use Phpro\SoapClient\Type\FactoryFromArray;
 
@@ -31,11 +28,34 @@ final class FactoryAssembler implements AssemblerInterface
     {
         $class = $context->getClass();
 
+        $traitAssembler = (new TraitAssembler(FactoryFromArray::class));
+
+        if ($traitAssembler->canAssemble($context)) {
+            $traitAssembler->assemble($context);
+        }
+
         try {
             $originalName = $class->getName();
+            $typeNamespace = str_replace('Factory', 'Type', $class->getNamespaceName());
             $class->setName($originalName . 'Factory');
-            $class->addTrait(FactoryFromArray::class);
+            $class->addUse($typeNamespace . '\\' . $originalName);
             
+            $class->setDocBlock(
+                DocBlockGeneratorFactory::fromArray(
+                    [
+                        'tags' => [
+                            [
+                                'name' => 'method',
+                                'description' => sprintf(
+                                    'static %s fromArray(array $array)',
+                                    $originalName
+                                )
+                            ]
+                        ],
+                    ]
+                )
+            );
+
             $class->addMethodFromGenerator(
                 MethodGenerator::fromArray(
                     [
@@ -44,8 +64,7 @@ final class FactoryAssembler implements AssemblerInterface
                         'name' => 'modelClass',
                         'returnType' => 'string',
                         'body' => sprintf(
-                            'return %s\%s::class;',
-                            str_replace($class->getNamespaceName(), 'Factory', 'Type'),
+                            'return %s::class;',
                             $originalName
                         ),
                     ]
