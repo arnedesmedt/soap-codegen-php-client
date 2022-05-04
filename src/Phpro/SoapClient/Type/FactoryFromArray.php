045832b6e02527cde200e8fe445726e32efb813a
Arne De Smedt <arnedesmdt@gmail.com>
2022-05-04 12:15:09 +0200

    add factory assembler

---
+++ src/Phpro/SoapClient/Type/FactoryFromArray.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phpro\SoapClient\Type;
+
+use EventEngine\JsonSchema\JsonSchemaAwareRecord;
+
+trait FactoryFromArray
+{
+    /**
+     * @return class-string<JsonSchemaAwareRecord>
+     */
+    abstract protected static function modelClass(): string;
+
+    /**
+     * @param array<string, mixed> $array
+     *
+     * @return mixed
+     */
+    public static function fromArray(array $array)
+    {
+        return (self::modelClass())::fromArray($array);
+    }
+}
